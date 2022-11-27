bb1fb56cbd9ade83420a21c7d67c545bef5340a9
Donguk Kim <ehddnr73@naver.com>
2022-11-28 00:51:21 +0900

    Playground project PlayOn_iOS 생성
    - 여러가지 SwiftUI 관련 학습 및 테스트

---
+++ PlayOn_iOS/PlayOn_iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PlayOn_iOS
+//
+//  Created by 김동욱 on 2022/11/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayOn_iOS/PlayOn_iOS/PlayOn_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayOn_iOSApp.swift
+//  PlayOn_iOS
+//
+//  Created by 김동욱 on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct PlayOn_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
