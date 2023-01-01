07ac3db4289265dbb39e987d20dbace776b9afdb
Donguk Kim <ehddnr73@naver.com>
2023-01-01 23:45:18 +0900

    한국과 일본의 인구 추이를 동시에 볼 수 있는 꺾은선 차트

---
+++ PlayOn_iOS/PlayOn_iOS/Findings/Charts/PopulationProgressLineCharts.swift
@@ -0,0 +1,43 @@
+//
+//  PopulationProgressLineCharts.swift
+//  PlayOn_iOS
+//
+//  Created by 김동욱 on 2023/01/01.
+//
+
+import SwiftUI
+import Charts
+
+struct Series: Identifiable {
+    let country: String
+    let populationSummary: [PopulationSummary]
+    
+    var id: String { country }
+}
+
+let seriesData: [Series] = [
+    .init(country: "Korea", populationSummary: koreaData),
+    .init(country: "Japan", populationSummary: japanData)
+]
+
+struct PopulationProgressLineCharts: View {
+    var body: some View {
+        Chart(seriesData) { series in
+            ForEach(series.populationSummary) { element in
+                LineMark(x: .value("Year", element.year),
+                         y: .value("Population", element.population)
+                )
+                // country에 따라 자동으로 색상을 구분해줌.
+                .foregroundStyle(by: .value("Country", series.country))
+                // PointMark를 중첩하는 효과
+                .symbol(by: .value("Country", series.country))
+            }
+        }
+    }
+}
+
+struct PopulationProgressLineCharts_Previews: PreviewProvider {
+    static var previews: some View {
+        PopulationProgressLineCharts()
+    }
+}
